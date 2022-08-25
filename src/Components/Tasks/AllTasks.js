ecb63ddfd533378616cce297652e258512727648
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-25 00:35:43 -0300

    Criado componente de tasks

--- src/Components/Tasks/AllTasks.js
+++ src/Components/Tasks/AllTasks.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import LiTasks from "./LiTasks";
+import styles from "./AllTasks.module.css";
+import Input from "../CreateTask/Input";
+import Button from "../CreateTask/Button";
+import { useState } from "react";
 
 const AllTasks = () => {
-  return (
-    <div>AllTasks</div>
-  )
-}
+  const [value, setValue] = useState("");
+  const [task, setTask] = useState([]);
 
-export default AllTasks
+  const handleTask = () => {
+    setTask([...task, value]);
+  };
+
+  return (
+    <div>
+      <div className={styles.add_task}>
+        <Input
+          className={styles.input}
+          type="text"
+          placeholder="Digite sua task"
+          onChange={({ target }) => setValue(target.value)}
+        />
+        <Button onClick={handleTask} text="Add task" />
+      </div>
+      
+      {task.length > 0 ? (
+        <ul className={styles.ul}>
+          {task.map((task) => (
+            <LiTasks task={task} />
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.p}>Sem tasks no momento..</p>
+      )}
+    </div>
+  );
+};
+
+export default AllTasks;
