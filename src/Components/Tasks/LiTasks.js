d9520a723e9894560f3a12de3aea4ec4d639abdc
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-08-29 19:51:00 -0300

    .

--- src/Components/Tasks/LiTasks.js
+++ src/Components/Tasks/LiTasks.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./LiTasks.module.css";
 import DeleteTask from "./DeleteTask";
+import { useCheckbox } from "../Hooks/useCheckbox";
 import Input from "../FormTask/Input";
 
+
 const LiTasks = ({ task, index, array, setTask }) => {
-  const [checkbox, setCheckbox] = useState(false);
+
+  const { checkbox, setCheckbox } = useCheckbox();
 
   return (
     <li className={styles.li}>
       <Input
         value="value"
         checked={checkbox}
-        onChange={({ target }) => setCheckbox(target.checked)}
-        type="checkbox"
+        onChange={({ target }) => setCheckbox(!checkbox)}
+        type='checkbox'
       />
-      <p className={checkbox ? 'complete' : ''}>{task}</p>
+      <p className={checkbox ? "complete" : ""}>{task}</p>
 
       <DeleteTask index={index} array={array} setTask={setTask} />
     </li>
